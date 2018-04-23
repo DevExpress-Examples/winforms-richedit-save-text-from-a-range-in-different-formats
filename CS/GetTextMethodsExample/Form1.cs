4189f093013522c8aa0b43d7918bbabc104cb7c8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GetTextMethodsExample/Form1.cs
@@ -0,0 +1,252 @@
+using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+
+namespace GetTextMethods {
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm {
+        string fileName = String.Empty;
+
+        public Form1() {
+            InitializeComponent();
+            richEditControl.LoadDocument("sample_document.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml); 
+        }
+
+        private void btnMht_Click(object sender, EventArgs e) {
+            this.fileName = GetFileName("Web Archive|*.mht");
+            string mhtText = String.Empty;
+
+            if (String.IsNullOrEmpty(fileName))
+                return;
+            try
+            {
+                #region #exportmht
+                if (this.richEditControl.Document.Selection.Length > 0) {
+                    DevExpress.XtraRichEdit.API.Native.DocumentRange selection = richEditControl.Document.Selection;
+                    DevExpress.XtraRichEdit.API.Native.SubDocument doc = selection.BeginUpdateDocument();
+                    mhtText = doc.GetMhtText(selection);
+                    selection.EndUpdateDocument(doc);  
+                }
+                else {
+                mhtText = richEditControl.Document.GetMhtText(richEditControl.Document.Range);
+                }
+                #endregion #exportmht
+                SaveFile(this.fileName, mhtText);
+                OpenFile(this.fileName);
+            }
+            finally {
+                this.fileName = String.Empty;
+            }
+        }
+        private void btnHtmlCustomUri_Click(object sender, EventArgs e) {
+            this.fileName = GetFileName("Hypertext Markup Language|*.html");
+            string htmlText = String.Empty;
+
+            if (String.IsNullOrEmpty(fileName))
+                return;
+            try {
+                #region #exporthtml
+                CustomUriProvider uriProvider = new CustomUriProvider(Path.GetDirectoryName(fileName));
+                if (this.richEditControl.Document.Selection.Length > 0) {
+                    DevExpress.XtraRichEdit.API.Native.DocumentRange selection = richEditControl.Document.Selection;
+                    DevExpress.XtraRichEdit.API.Native.SubDocument doc = selection.BeginUpdateDocument();
+                    htmlText = doc.GetHtmlText(selection, uriProvider);
+                    selection.EndUpdateDocument(doc);
+                }
+                else {
+                    htmlText = richEditControl.Document.GetHtmlText(richEditControl.Document.Range, uriProvider);
+                }
+                #endregion #exporthtml
+                SaveFile(this.fileName, htmlText);
+                OpenFile(this.fileName);
+            }
+            finally {
+                this.fileName = String.Empty;
+            }
+        }
+
+        private void btnHtmlOptions_Click(object sender, EventArgs e) {
+            #region #exporthtmloptions
+            frmBrowser myBrowser = new frmBrowser();
+            DevExpress.XtraRichEdit.Export.HtmlDocumentExporterOptions myExportOptions =
+                new DevExpress.XtraRichEdit.Export.HtmlDocumentExporterOptions();
+            myExportOptions.Encoding = System.Text.Encoding.Unicode;
+
+            if (this.richEditControl.Document.Selection.Length > 0) {
+                DevExpress.XtraRichEdit.API.Native.DocumentRange selection = richEditControl.Document.Selection;
+                DevExpress.XtraRichEdit.API.Native.SubDocument doc = selection.BeginUpdateDocument();
+                myBrowser.SetHtml(doc.GetHtmlText(selection, null, myExportOptions));
+                selection.EndUpdateDocument(doc);
+            }
+            else {
+                myBrowser.SetHtml(richEditControl.Document.GetHtmlText(richEditControl.Document.Range, null, myExportOptions));
+            }
+
+            myBrowser.Show();
+        }
+        #endregion #exporthtmloptions
+        private void btnDocx_Click(object sender, EventArgs e) {
+            this.fileName = GetFileName("Word 2007 Document|*.docx");
+            byte[] bytes;
+
+            if (String.IsNullOrEmpty(fileName))
+                return;
+            try
+            {
+                #region #exportdocx
+                if (this.richEditControl.Document.Selection.Length > 0) {
+                    DevExpress.XtraRichEdit.API.Native.DocumentRange selection = richEditControl.Document.Selection;
+                    DevExpress.XtraRichEdit.API.Native.SubDocument doc = selection.BeginUpdateDocument();
+                    bytes = doc.GetOpenXmlBytes(selection);
+                    selection.EndUpdateDocument(doc);
+                }
+                else {
+                    bytes = richEditControl.Document.GetOpenXmlBytes(richEditControl.Document.Range);
+                }
+                #endregion #exportdocx
+                SaveFile(this.fileName, bytes);
+                OpenFile(this.fileName);
+            }
+            finally {
+                this.fileName = String.Empty;
+            }
+        }
+        private void btnRtf_Click(object sender, EventArgs e) {
+            this.fileName = GetFileName("Rich Text Format|*.rtf");
+            string rtfText = String.Empty;
+
+            if (String.IsNullOrEmpty(fileName))
+                return;
+            try
+            {
+                #region #exportrtf
+                if (this.richEditControl.Document.Selection.Length > 0) {
+                    DevExpress.XtraRichEdit.API.Native.DocumentRange selection = richEditControl.Document.Selection;
+                    DevExpress.XtraRichEdit.API.Native.SubDocument doc = selection.BeginUpdateDocument();
+                    rtfText = doc.GetRtfText(selection);
+                    selection.EndUpdateDocument(doc);
+                }
+                else {
+                    rtfText = richEditControl.Document.GetRtfText(richEditControl.Document.Range);
+                }
+                #endregion #exportrtf
+                SaveFile(this.fileName, rtfText);
+                OpenFile(this.fileName);
+            }
+            finally {
+                this.fileName = String.Empty;
+            }
+        }
+
+        private void btnXml_Click(object sender, EventArgs e) {
+            this.fileName = GetFileName("Microsoft Word XML Document|*.xml");
+            string wordMLText = String.Empty;
+
+            if (String.IsNullOrEmpty(fileName))
+                return;
+            try
+            {
+                #region #exportxml
+                if (this.richEditControl.Document.Selection.Length > 0) {
+                    DevExpress.XtraRichEdit.API.Native.DocumentRange selection = richEditControl.Document.Selection;
+                    DevExpress.XtraRichEdit.API.Native.SubDocument doc = selection.BeginUpdateDocument();
+                    wordMLText = doc.GetWordMLText(selection);
+                    selection.EndUpdateDocument(doc);
+                }
+                else {
+                    wordMLText = richEditControl.Document.GetWordMLText(richEditControl.Document.Range);
+                }
+                #endregion #exportxml
+                SaveFile(this.fileName, wordMLText);
+                OpenFile(this.fileName);
+            }
+            finally {
+                this.fileName = String.Empty;
+            }
+        }
+
+
+
+        private void SaveFile(string fileName, string value) {
+            using (FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write)) {
+                using (StreamWriter writer = new StreamWriter(stream)) {
+                    writer.Write(value);
+                }
+            }
+        }
+        private void SaveFile(string fileName, byte[] bytes) {
+            using (FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write)) {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+        private string GetFileName(string filter) {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog()) {
+                saveFileDialog.Filter = filter;
+                saveFileDialog.RestoreDirectory = true;
+                saveFileDialog.CheckFileExists = false;
+                saveFileDialog.CheckPathExists = true;
+                saveFileDialog.OverwritePrompt = true;
+                saveFileDialog.DereferenceLinks = true;
+                saveFileDialog.ValidateNames = true;
+                if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+                    return saveFileDialog.FileName;
+            }
+            return String.Empty;
+        }
+        private void OpenFile(string fileName) {
+            if (MessageBox.Show("File is saved. \nDo you want to open this file?", "GetText Example", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+                Process process = new Process();
+                try {
+                    process.StartInfo.FileName = fileName;
+                    process.Start();
+                }
+                catch { }
+            }
+        }
+        #region #beforeexport
+        private void richEditControl_BeforeExport(object sender, DevExpress.XtraRichEdit.BeforeExportEventArgs e) {
+            DevExpress.XtraRichEdit.Export.HtmlDocumentExporterOptions options = e.Options as DevExpress.XtraRichEdit.Export.HtmlDocumentExporterOptions;
+            if (options != null) {
+                options.CssPropertiesExportType = DevExpress.XtraRichEdit.Export.Html.CssPropertiesExportType.Link;
+                options.HtmlNumberingListExportFormat = DevExpress.XtraRichEdit.Export.Html.HtmlNumberingListExportFormat.HtmlFormat;
+                options.TargetUri = Path.GetFileNameWithoutExtension(this.fileName);
+            }
+        }
+        #endregion #beforeexport
+
+
+    }
+
+    #region #uriprovider
+    public class CustomUriProvider : DevExpress.Office.Services.IUriProvider {
+    string rootDirecory;
+    public CustomUriProvider(string rootDirectory) {
+        if (String.IsNullOrEmpty(rootDirectory))
+            DevExpress.Office.Utils.Exceptions.ThrowArgumentException("rootDirectory", rootDirectory);
+        this.rootDirecory = rootDirectory;
+    }
+
+    public string CreateCssUri(string rootUri, string styleText, string relativeUri) {
+        string cssDir = String.Format("{0}\\{1}", this.rootDirecory, rootUri.Trim('/'));
+        if (!Directory.Exists(cssDir))
+            Directory.CreateDirectory(cssDir);
+        string cssFileName = String.Format("{0}\\style.css", cssDir);
+        File.AppendAllText(cssFileName, styleText);
+        return GetRelativePath(cssFileName);
+    }
+        public string CreateImageUri(string rootUri, DevExpress.Office.Utils.OfficeImage image, string relativeUri) {
+        string imagesDir = String.Format("{0}\\{1}", this.rootDirecory, rootUri.Trim('/'));
+        if (!Directory.Exists(imagesDir))
+            Directory.CreateDirectory(imagesDir);
+        string imageName = String.Format("{0}\\{1}.png", imagesDir, Guid.NewGuid());
+        image.NativeImage.Save(imageName, ImageFormat.Png);
+        return GetRelativePath(imageName);
+    }
+    string GetRelativePath(string path) {
+        string substring = path.Substring(this.rootDirecory.Length);
+        return substring.Replace("\\", "/").Trim('/');
+    }
+}
+    #endregion #uriprovider
+}
---
+++ VB/GetTextMethodsExample/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿Namespace GetTextMethods
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.btnMht = New DevExpress.XtraEditors.SimpleButton()
+            Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+            Me.btnHtmlOptions = New DevExpress.XtraEditors.SimpleButton()
+            Me.btnXml = New DevExpress.XtraEditors.SimpleButton()
+            Me.labelControl1 = New DevExpress.XtraEditors.LabelControl()
+            Me.btnRtf = New DevExpress.XtraEditors.SimpleButton()
+            Me.btnDocx = New DevExpress.XtraEditors.SimpleButton()
+            Me.btnHtmlCustomUri = New DevExpress.XtraEditors.SimpleButton()
+            Me.richEditControl = New DevExpress.XtraRichEdit.RichEditControl()
+            DirectCast(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.panelControl1.SuspendLayout()
+            Me.SuspendLayout()
+            ' 
+            ' btnMht
+            ' 
+            Me.btnMht.Location = New System.Drawing.Point(60, 5)
+            Me.btnMht.Name = "btnMht"
+            Me.btnMht.Size = New System.Drawing.Size(50, 23)
+            Me.btnMht.TabIndex = 1
+            Me.btnMht.Text = "Mht"
+            ' 
+            ' panelControl1
+            ' 
+            Me.panelControl1.Controls.Add(Me.btnHtmlOptions)
+            Me.panelControl1.Controls.Add(Me.btnXml)
+            Me.panelControl1.Controls.Add(Me.labelControl1)
+            Me.panelControl1.Controls.Add(Me.btnRtf)
+            Me.panelControl1.Controls.Add(Me.btnDocx)
+            Me.panelControl1.Controls.Add(Me.btnHtmlCustomUri)
+            Me.panelControl1.Controls.Add(Me.btnMht)
+            Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.panelControl1.Location = New System.Drawing.Point(0, 0)
+            Me.panelControl1.Name = "panelControl1"
+            Me.panelControl1.Size = New System.Drawing.Size(963, 64)
+            Me.panelControl1.TabIndex = 5
+            ' 
+            ' btnHtmlOptions
+            ' 
+            Me.btnHtmlOptions.Location = New System.Drawing.Point(127, 34)
+            Me.btnHtmlOptions.Name = "btnHtmlOptions"
+            Me.btnHtmlOptions.Size = New System.Drawing.Size(163, 23)
+            Me.btnHtmlOptions.TabIndex = 7
+            Me.btnHtmlOptions.Text = "Html using export options"
+            ' 
+            ' btnXml
+            ' 
+            Me.btnXml.Location = New System.Drawing.Point(441, 5)
+            Me.btnXml.Name = "btnXml"
+            Me.btnXml.Size = New System.Drawing.Size(50, 23)
+            Me.btnXml.TabIndex = 6
+            Me.btnXml.Text = "Xml"
+            ' 
+            ' labelControl1
+            ' 
+            Me.labelControl1.Location = New System.Drawing.Point(12, 10)
+            Me.labelControl1.Name = "labelControl1"
+            Me.labelControl1.Size = New System.Drawing.Size(42, 13)
+            Me.labelControl1.TabIndex = 5
+            Me.labelControl1.Text = "Save as:"
+            ' 
+            ' btnRtf
+            ' 
+            Me.btnRtf.Location = New System.Drawing.Point(374, 5)
+            Me.btnRtf.Name = "btnRtf"
+            Me.btnRtf.Size = New System.Drawing.Size(50, 23)
+            Me.btnRtf.TabIndex = 4
+            Me.btnRtf.Text = "Rtf"
+            ' 
+            ' btnDocx
+            ' 
+            Me.btnDocx.Location = New System.Drawing.Point(307, 5)
+            Me.btnDocx.Name = "btnDocx"
+            Me.btnDocx.Size = New System.Drawing.Size(50, 23)
+            Me.btnDocx.TabIndex = 3
+            Me.btnDocx.Text = "Docx"
+            ' 
+            ' btnHtmlCustomUri
+            ' 
+            Me.btnHtmlCustomUri.Location = New System.Drawing.Point(127, 5)
+            Me.btnHtmlCustomUri.Name = "btnHtmlCustomUri"
+            Me.btnHtmlCustomUri.Size = New System.Drawing.Size(163, 23)
+            Me.btnHtmlCustomUri.TabIndex = 2
+            Me.btnHtmlCustomUri.Text = "Html using custom UriProvider"
+            ' 
+            ' richEditControl
+            ' 
+            Me.richEditControl.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.richEditControl.EnableToolTips = True
+            Me.richEditControl.Location = New System.Drawing.Point(0, 64)
+            Me.richEditControl.Name = "richEditControl"
+            Me.richEditControl.Options.Bookmarks.AllowNameResolution = False
+            Me.richEditControl.Options.Export.PlainText.ExportFinalParagraphMark = DevExpress.XtraRichEdit.Export.PlainText.ExportFinalParagraphMark.Never
+            Me.richEditControl.Size = New System.Drawing.Size(963, 460)
+            Me.richEditControl.TabIndex = 6
+            Me.richEditControl.Text = "richEditControl1"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(963, 524)
+            Me.Controls.Add(Me.richEditControl)
+            Me.Controls.Add(Me.panelControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.panelControl1.ResumeLayout(False)
+            Me.panelControl1.PerformLayout()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private WithEvents btnMht As DevExpress.XtraEditors.SimpleButton
+        Private panelControl1 As DevExpress.XtraEditors.PanelControl
+        Private WithEvents btnHtmlCustomUri As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents btnDocx As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents btnRtf As DevExpress.XtraEditors.SimpleButton
+        Private labelControl1 As DevExpress.XtraEditors.LabelControl
+        Private WithEvents btnXml As DevExpress.XtraEditors.SimpleButton
+        Private richEditControl As DevExpress.XtraRichEdit.RichEditControl
+        Private WithEvents btnHtmlOptions As DevExpress.XtraEditors.SimpleButton
+    End Class
+End Namespace
+
